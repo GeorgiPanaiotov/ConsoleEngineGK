474613e30eb3c9026c795343b8f41d671da44012
GeorgiPanaiotov <rippness@gmail.com>
2022-02-05 14:47:23 +0200

    Added colors, drawing rectangles and changing app name

--- ConsoleEngineGK.h
+++ ConsoleEngineGK.h
@@ -3,6 +3,9 @@
 #include <string>
 
 
+
+
+
 class ConsoleEngineGK
 {
 private:
@@ -24,6 +27,42 @@ public:
 		appName = L"Default";
 	};
 
+	enum COLORS
+	{
+		BLACK_FG = 0x0000,
+		DARK_BLUE_FG = 0x0001,
+		DARK_GREEN_FG = 0x0002,
+		DARK_CYAN_FG = 0x0003,
+		DARK_RED_FG = 0x0004,
+		DARK_MAGENTA_FG = 0x0005,
+		DARK_YELLOW_FG = 0x0006,
+		GREY_FG = 0x0007,
+		DARK_GREY_FG = 0x0008,
+		BLUE_FG = 0x0009,
+		GREEN_FG = 0x000A,
+		CYAN_FG = 0x000B,
+		RED_FG = 0x000C,
+		MAGENTA_FG = 0x000D,
+		YELLOW_FG = 0x000E,
+		WHITE_FG = 0x000F,
+		BLACK_BG = 0x0000,
+		DARK_BLUE_BG = 0x0010,
+		DARK_GREEN_BG = 0x0020,
+		DARK_CYAN_BG = 0x0030,
+		DARK_RED_BG = 0x0040,
+		DARK_MAGENTA_BG = 0x0050,
+		DARK_YELLOW_BG = 0x0060,
+		GREY_BG = 0x0070,
+		DARK_GREY_BG = 0x0080,
+		BLUE_BG = 0x0090,
+		GREEN_BG = 0x00A0,
+		CYAN_BG = 0x00B0,
+		RED_BG = 0x00C0,
+		MAGENTA_BG = 0x00D0,
+		YELLOW_BG = 0x00E0,
+		WHITE_BG = 0x00F0,
+	};
+
 	void CreateConsole(int width, int height)
 	{
 		consoleWidth = width;
@@ -42,16 +81,74 @@ public:
 		memset(screenBuffer, 0, sizeof(CHAR_INFO) * consoleWidth * consoleHeight);
 	};
 
-	void DrawString(int x, int y, std::string c, short col = 0x000F)
+	void DrawString(int x, int y, std::string c, short color = 0x000F)
 	{
 		for (int i = 0; i < c.length(); i++)
 		{
 			screenBuffer[y * consoleWidth + x + i].Char.UnicodeChar = c[i];
-			screenBuffer[y * consoleWidth + x + i].Attributes = col;
+			screenBuffer[y * consoleWidth + x + i].Attributes = color;
 		}
 		Draw();
 	}
 
+	void FillColor(short col)
+	{
+		for (int i = 0; i < consoleWidth * consoleHeight; i++)
+		{
+			screenBuffer[i].Char.UnicodeChar = 0x2591;
+			screenBuffer[i].Attributes = col;
+		}
+		Draw();
+	};
+
+	void DrawRect(int x, int y, int endX, int endY, short color = 0x000F)
+	{
+
+		int tempTopY = y;
+		int tempTopX = tempTopY * consoleWidth + x;
+		int currentX = x;
+
+		for (int i = 0; i < consoleWidth * consoleHeight; i++)
+		{
+			for (int j = 0; j < consoleWidth * consoleHeight; j++)
+			{
+				if(currentX == endX)
+				{
+					currentX = x;
+					break;
+				}
+				if (tempTopX <= tempTopY * consoleWidth + endX)
+				{
+					screenBuffer[tempTopX].Char.UnicodeChar = 0x2591;
+					screenBuffer[tempTopX].Attributes = color;
+					tempTopX++;
+					currentX++;
+				}
+			}
+			if (tempTopY == endY) {
+				break;
+			}
+			tempTopX = tempTopY * consoleWidth + x;
+			tempTopY++;
+		};
+
+		Draw();
+	};
+
+	void SetAppName(std::string appName)
+	{
+		wchar_t convAppName[256] = { 0 };
+		memset(convAppName, 0, 256);
+		for (int i = 0; i < appName.length(); i++)
+		{
+			convAppName[i] = appName[i];
+		}
+		SetConsoleTitle(convAppName);
+	};
+
+
+
+protected:
 	void Draw()
 	{
 		WriteConsoleOutput(consoleHandle, screenBuffer, { (short)consoleWidth, (short)consoleHeight }, { 0,0 }, &rectWnd);
